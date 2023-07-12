de7feeb9635df41dfbc58c9802ea0814ad54018e
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-07-12 17:47:07 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domainName").innerHTML = getDomain();
 };
+
+let pronouns = ["the", "our", "his", "my"];
+let adjectives = ["great", "big", "small", "tallest"];
+let nouns = ["jogger", "raccoon", "dog", "cat"];
+let extensions = [".com", ".gov", ".us", ".net"];
+
+function getDomain() {
+  let domain = "";
+  for (let pronounIndex = 0; pronounIndex < pronouns.length; pronounIndex++) {
+    for (
+      let adjectiveIndex = 0;
+      adjectiveIndex < adjectives.length;
+      adjectiveIndex++
+    ) {
+      for (let nounIndex = 0; nounIndex < nouns.length; nounIndex++) {
+        for (let extIndex = 0; extIndex < extensions.length; extIndex++) {
+          domain +=
+            pronouns[pronounIndex] +
+            adjectives[adjectiveIndex] +
+            nouns[nounIndex] +
+            extensions[extIndex] +
+            "<br>";
+        }
+      }
+    }
+  }
+  return domain;
+}
+
+let domainName = getDomain();
